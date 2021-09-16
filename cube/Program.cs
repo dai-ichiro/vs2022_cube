523623b548c1f075932cbfb1a4ef6c200c52a5c3
dai-ichiro <d.a.i-ichiro@outlook.jp>
2021-09-16 21:48:41 +0900

    update code

--- cube/Program.cs
+++ cube/Program.cs
@@ -12,8 +12,8 @@ class Program
     {
         Console.WriteLine("Loading pretrained data...");
 
-        //scramble = "R' U' F R' B' F2 L2 D' U' L2 F2 D' L2 D' R B D2 L D2 F2 U2 L R' U' F";
-        scramble = "R' U' F R' B' F2 L2 D' U' L2 F2 D' L2 D' R B";
+        scramble = "R' U' F R' B' F2 L2 D' U' L2 F2 D' L2 D' R B D2 L D2 F2 U2 L R' U' F";
+        //scramble = "R' U' F R' B' F2 L2 D' U' L2 F2 D' L2 D' R B";
         //scramble = "R' U' F R' B' F2 L2 D' U' L2 F2 D' L2 D' R";
         //scramble = "R' U' F R' B' F2 L2 D' U' L2 F2 D' L2";
         //scramble = "R' U' F R' B' F2 L2 D' U'";
